72c5df3cebc37efefe940ba1ca51cda2a4e0df16
tabuna <bliz48rus@gmail.com>
2024-03-28 02:31:53 +0300

    refs #106 Fixed avatar sharp circle

--- resources/views/comments/show.blade.php
+++ resources/views/comments/show.blade.php
@@ -1,6 +1,6 @@
 <x-comment :comment="$comment">
     <x-slot:content>
-        <p class="my-3 text-wrap text-break">{!! $comment->prettyComment() !!}</p>
+        <p class="my-3 text-wrap text-break message">{!! $comment->prettyComment() !!}</p>
     </x-slot:content>
 </x-comment>
 
