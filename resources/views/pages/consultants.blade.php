4bb26e12326d362e1c5e71c2afab05ecf22d30b8
tabuna <bliz48rus@gmail.com>
2024-04-09 23:55:06 +0300

    wip

--- resources/views/pages/consultants.blade.php
+++ resources/views/pages/consultants.blade.php
@@ -13,7 +13,7 @@
         </x-slot>
 
         <x-slot name="description">
-            Передайте проект на аутсорсинг или привлеките экспертов Laravel в свою существующую команду.
+            Каждое изменение должно приносить результат. Консультации помогут вам определить, как это достичь.
         </x-slot>
 
 
@@ -39,7 +39,7 @@
 
 
 
-
+{{--
     <x-container>
         <div class="bg-body-tertiary rounded overflow-hidden p-4 p-xxl-5 position-relative">
 
@@ -54,7 +54,7 @@
                                     <small
                                         class="text-uppercase ls-md fw-semibold text-body-tertiary">Зачем?</small>
                                     <h2 class="mt-3">
-                                        Lorem ipsum dolor sit amet. <span class="text-primary">Suspendisse</span> sed eros mi.
+                                        Мы знаем потребности вашего бизнеса
                                     </h2>
                                 </div>
                             </div>
@@ -182,19 +182,96 @@
                     </p>
                 </div>
             </div>
-            --}}
+
         </div>
     </x-container>
+    --}}
 
     <x-container>
 
+        {{--
         <div class="row g-4 g-md-5 justify-content-center align-items-end mb-5">
             <div class="col-lg-8 me-auto">
-                <span class="text-primary mb-3 d-block text-uppercase fw-semibold ls-xl">Помогите вашей команде двигаться вперёд.</span>
+                <span class="text-primary mb-3 d-block text-uppercase fw-semibold ls-xl">Чего вы можете ожидать от консультанта Laravel</span>
                 <h2 class="display-5 fw-semibold mb-0">Чего вы можете ожидать от консультанта Laravel</h2>
             </div>
         </div>
+--}}
 
+        <div class="row g-4 row-cols-md-2 mb-5 text-balance">
+            <div class="col">
+                <div class="p-4 p-xl-5 bg-body-secondary rounded-3 position-relative mb-4 h-100">
+                    <div class="d-flex flex-column g-4 g-md-5">
+                        <div class="d-flex mb-4 align-items-center gap-5">
+                            <h3 class="display-6 fw-semibold mb-0 text-balance">Застряли на проблеме?</h3>
+                            <div class="d-none d-sm-flex">
+                                <x-icon path="i.code" class="img-fluid d-block mx-auto text-primary" width="4rem"
+                                        height="4rem"/>
+                            </div>
+                        </div>
+                        <p class="col-md-10">
+                            Специалисты предлагают свежий взгляд, который поможет вам разобраться.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="p-4 p-xl-5 bg-body-secondary rounded-3 position-relative mb-4 h-100">
+                    <div class="d-flex flex-column g-4 g-md-5">
+                        <div class="d-flex mb-4 align-items-center gap-5">
+                            <h3 class="display-6 fw-semibold mb-0 text-balance">Погружение в код</h3>
+                            <div class="d-none d-sm-flex">
+                                <x-icon path="i.code" class="img-fluid d-block mx-auto text-primary" width="4rem"
+                                        height="4rem"/>
+                            </div>
+                        </div>
+                        <p class="col-md-10">
+                            Давайте вместе рассмотрим ваш код, чтобы убедиться, что он максимально хорош.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="p-4 p-xl-5 bg-body-secondary rounded-3 position-relative mb-4 h-100">
+
+                    <div class="d-flex flex-column g-4 g-md-5">
+
+                        <div class="d-flex mb-4 align-items-center gap-5">
+                            <h3 class="display-6 fw-semibold mb-0 text-balance">Разбор архитектуры</h3>
+                            <div class="d-none d-sm-flex">
+                                <x-icon path="i.ui" class="img-fluid d-block mx-auto text-primary" width="4rem"
+                                        height="4rem"/>
+                            </div>
+                        </div>
+                        <p class="col-md-10">
+                            Мы обсудим, как правильно структурировать ваше приложение для достижения успеха.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="p-4 p-xl-5 bg-body-secondary rounded-3 position-relative mb-4 h-100">
+                    <div class="d-flex flex-column g-4 g-md-5">
+
+                        <div class="d-flex mb-4 align-items-center gap-5">
+                            <h3 class="display-6 fw-semibold mb-0 text-balance">Лучшие практики</h3>
+                            <div class="d-none d-sm-flex">
+                                <x-icon path="i.utilities" class="img-fluid d-block mx-auto text-primary" width="4rem"
+                                        height="4rem"/>
+                            </div>
+                        </div>
+                        <p class="col-md-10">
+                            Мы поговорим о новейших передовых практиках, которые помогут вашему проекту не сбиться с пути.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        {{--
             <div class="row g-4 row-cols-md-2 row-cols-xxl-3">
                 <div class="col">
                     <div class="h-100 d-flex flex-md-row flex-column px-4 py-3 py-xl-5 bg-body-secondary rounded position-relative align-items-md-center align-items-xxl-baseline">
@@ -208,7 +285,7 @@
                 <div class="col">
                     <div class="h-100 d-flex flex-md-row flex-column px-4 py-3 py-xl-5 bg-body-secondary rounded position-relative align-items-md-center align-items-xxl-baseline">
                         <div>
-                            <p class="fs-4">Обзор кода</p>
+                            <p class="fs-4">Обзор вашего кода</p>
 
                             <p class="text-body-secondary mb-0">Давайте вместе рассмотрим ваш код, чтобы убедиться, что он максимально хорош.</p>
                         </div>
@@ -275,10 +352,106 @@
                         </div>
                     </div>
                 </div>
-                --}}
+
             </div>
+        --}}
     </x-container>
 
+
+    <x-container>
+
+
+        <!--Hire jamstack start-->
+        <section class="p-4 p-xl-5 bg-body-secondary rounded position-relative">
+            <figure class="position-absolute top-0 start-0 translate-middle z-n1 ms-4">
+                <x-icon path="l.cube" width="46" height="53" fill="none"/>
+            </figure>
+
+                <div class="row">
+                    <div class="col-lg-11 mx-auto col-md-12">
+                        <div class="row align-items-center g-5">
+                            <div class="col-lg-6 col-12 order-2">
+                                <div class="me-xl-7">
+                                    <div class="mb-4">
+                                        <span class="text-primary mb-3 d-block text-uppercase fw-semibold ls-xl">Мы знаем</span>
+                                        <p class="display-6 fw-bold text-balance">Потребности и способы решения</p>
+
+                                    </div>
+                                    <div class="mb-5">
+                                        <ul>
+                                            <li class="mb-3">
+                                                Чем больше вы погружаетесь в код, тем сложнее заметить проблемы. Взгляд со
+                                                стороны может принести новые идеи и перспективы, которые изменят вашу игру.
+                                            </li>
+                                            <li class="mb-3">
+                                                Отдавайте часть ответственности за код сообществу открытого исходного кода,
+                                                так вы сможете достигать больших результатов с меньшим объемом работы.
+                                            </li>
+                                        </ul>
+                                    </div>
+
+                                    <div class="row align-items-center company-usage">
+                                        <div class="col-lg-6 col-md-6 col-6">
+                                            <img src="./img/sponsors/am.svg"
+                                                 alt="logo" class="img-fluid" />
+                                        </div>
+                                        <div class="col-lg-6 col-md-6 col-6">
+                                            <img src="/img/sponsors/soidet.svg"
+                                                 alt="logo" class="img-fluid" />
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-12 order-lg-2">
+                                <div class="bg-body-tertiary p-4 p-xxl-5 shadow-sm rounded">
+                                        <form class="row needs-validation g-3" novalidate>
+                                            <div class="col-12">
+                                                <label for="ScheduleFirstnameInput" class="form-label">
+                                                    Как вас зовут?
+                                                    <span class="text-danger">*</span>
+                                                </label>
+                                                <input type="text"
+                                                       placeholder="По имени всегда приятно"
+                                                       class="form-control"
+                                                       id="ScheduleFirstnameInput"
+                                                       required />
+                                            </div>
+                                            <div class="col-md-12">
+                                                <label for="scheduleEmailInput"
+                                                       class="form-label">
+                                                    Как связаться?
+                                                    <span
+                                                        class="text-danger">*</span>
+                                                </label>
+                                                <input type="text"
+                                                       placeholder="Телеграм или email для связи"
+                                                       class="form-control"
+                                                       id="scheduleEmailInput"
+                                                       required />
+                                            </div>
+                                            <div class="col-md-12">
+                                                <label for="scheduleTextarea"
+                                                       class="form-label">Тема</label>
+                                                <textarea class="form-control"
+                                                          id="scheduleTextarea"
+                                                          placeholder="Расскажите какую проблему вы хотели бы решить, так мы сможем подобрать лучшего специалиста."
+                                                          rows="8" required></textarea>
+                                            </div>
+                                            <div class="d-grid">
+                                                <button class="btn btn-primary"
+                                                        type="submit">Получить консультацию</button>
+                                            </div>
+                                        </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </section>
+        <!--Hire jamstack end-->
+
+    </x-container>
 
     <x-container>
         <div class="row g-4 g-md-5 justify-content-center align-items-end mb-5">
