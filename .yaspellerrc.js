00b3de1c9fdcd23d370c59dd5812c713f2327679
Alexandr Chernyaev <bliz48rus@gmail.com>
2024-07-28 23:55:35 +0300

    Fixed yaspellerrc

---
+++ .yaspellerrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    excludeFiles: [
+        ".git",
+    ],
+    format: "auto",
+    fileExtensions: [
+        ".json",
+        ".markdown",
+        ".md"
+    ],
+    ignoreUrls: true,
+    ignoreDigits: true,
+    checkYo: false,
+    dictionary: [
+        'сессий',
+        'токена',
+        'Предотвратите',
+        'XXE',
+        'XEE',
+        'перенаправлению',
+        'Codeable'
+    ],
+};
