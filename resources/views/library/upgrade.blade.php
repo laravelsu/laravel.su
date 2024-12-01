61ca89fc348939a55427c93c8e46073164187b08
Alexandr Chernyaev <bliz48rus@gmail.com>
2024-12-01 04:51:12 +0300

    Automatically display the latest version on the library upgrade page.

--- resources/views/library/upgrade.blade.php
+++ resources/views/library/upgrade.blade.php
@@ -12,15 +12,26 @@
             которого необходимо придерживаться.
         </x-slot>
 
-        <x-slot:content >
+        <x-slot:content>
             <div class="d-none d-md-flex align-items-baseline lead fw-bold mx-md-auto text-md-center">
-                <span class="d-inline-block mx-2 mx-sm-3 display-5 opacity-25">7.x</span>
-                <span class="d-inline-block mx-2 mx-sm-3 display-4 opacity-50 border-5">8.x</span>
-                <span class="d-inline-block mx-2 mx-sm-3 display-3 opacity-75">9.x</span>
-                <span class="d-inline-block mx-2 mx-sm-3 display-1 border-bottom border-primary">10.x</span>
+                @php
+                    $version = \Str::of(\App\Docs::DEFAULT_VERSION)->before('.')->toString();
+                @endphp
+
+                @foreach(collect()->range(0,3)->reverse() as $index)
+                    <span @class([
+                    'd-inline-block mx-2 mx-sm-3',
+                    'display-' . ($index + 1),
+                    'opacity-' . [100, 75, 50, 25][$index],
+                    'border-bottom border-primary' => $index === 0,
+                ])>
+                    {{ $version - $index }}.x
+                </span>
+                @endforeach
             </div>
         </x-slot:content>
     </x-header>
+
 
 @php
     $sections = collect([
