62673c212300d6a8f5b89498bce020eed5705532
Alexandr Chernyaev <bliz48rus@gmail.com>
2024-12-11 05:42:15 +0300

    Missing commit for  Secret Santa (#256)

--- routes/web.php
+++ routes/web.php
@@ -484,6 +484,21 @@ Route::get('/santa/rules', [\App\Http\Controllers\SantaController::class, 'rules
 
 /*
 |--------------------------------------------------------------------------
+| Secret Santa
+|--------------------------------------------------------------------------
+*/
+Route::middleware(['auth'])
+    ->group(function () {
+        Route::get('/santa/game', [\App\Http\Controllers\SantaController::class, 'game'])->name('santa.game');
+        Route::post('/santa/game', [\App\Http\Controllers\SantaController::class, 'update'])->name('santa.update');
+        Route::delete('/santa/game', [\App\Http\Controllers\SantaController::class, 'delete'])->name('santa.delete');
+    });
+
+Route::get('/santa', [\App\Http\Controllers\SantaController::class, 'index'])->name('santa');
+Route::get('/santa/rules', [\App\Http\Controllers\SantaController::class, 'rules'])->name('santa.rules');
+
+/*
+|--------------------------------------------------------------------------
 | Review
 |--------------------------------------------------------------------------
 */
