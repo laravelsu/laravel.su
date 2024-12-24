25a979e24889882ea45bd1346d69f000753bb2ac
Alexandr Chernyaev <bliz48rus@gmail.com>
2024-12-24 18:23:32 +0300

    Added call `filters()` for apply sort

--- app/Orchid/Screens/SecretSantaScreen.php
+++ app/Orchid/Screens/SecretSantaScreen.php
@@ -24,6 +24,7 @@ class SecretSantaScreen extends Screen
     {
         return [
             'participants' => SecretSantaParticipant::with(['receiver', 'santa', 'user'])
+                ->filters()
                 ->get(),
         ];
     }
