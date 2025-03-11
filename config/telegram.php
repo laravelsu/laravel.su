d64ca6b2e4533c51fadedc9f13c6e5ad91fa4743
Babaiev Ihor <75082815+bald-cat@users.noreply.github.com>
2025-03-11 15:53:54 +0200

    Tg captcha (#275)
    * added captcha for tg bot
    
    * added locale for captcha
    
    * added locale for captcha

---
+++ config/telegram.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'chats' => [
+        [
+            'id'     => '-1002111700088',
+            'name'   => 'Ахтунг laravel.su чат',
+            'locale' => 'en',
+        ],
+        [
+            'id'     => '-1001300166722',
+            'name'   => 'Laravel Orchid Russian Community',
+            'locale' => 'ru',
+        ],
+        [
+            'id'     => '-1001450006147',
+            'name'   => 'Laravel Orchid',
+            'locale' => 'en',
+        ],
+        [
+            'id'     => '-1001104353296',
+            'name'   => 'Laravel Framework Russian Community',
+            'locale' => 'ru',
+        ],
+    ],
+    'default_locale' => 'ru',
+];
