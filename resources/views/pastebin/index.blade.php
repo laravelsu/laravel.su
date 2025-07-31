656a27b3681cb3be6404cd6a332b918c4d6c36f0
Artyom Osepyan <152782500+artengin@users.noreply.github.com>
2025-07-31 10:49:36 +0300

    bug: Fixed empty code submission in Pastebin. Closes #265 (#285)
    * bug: Fixed empty code submission in Pastebin. Closes #265
    
    * Update index.blade.php
    
    ---------
    
    Co-authored-by: Alexandr Chernyaev <bliz48rus@gmail.com>

--- resources/views/pastebin/index.blade.php
+++ resources/views/pastebin/index.blade.php
@@ -4,40 +4,54 @@
 @section('description', 'Делитесь вашим кодом онлайн с кем угодно на земле.')
 
 @section('content')
-
     <x-container>
-
         <form action="{{ route('pastebin.store') }}" method="post">
             <div data-controller="prism" class="rounded overflow-hidden">
-            <pre contenteditable="true"
-                 class="rounded line-numbers border p-4 m-0"
-                 style="min-height: 600px;"
-                 data-action="input->prism#paste keydown->prism#keydownPaste"
-                 data-prism-target="editable"><code id="yaml"
-                                                    placeholder="Поделитесь своим фрагментом кода тут!">{{ $content }}</code></pre>
+                <pre contenteditable="true"
+                     class="rounded line-numbers border p-4 m-0"
+                     style="min-height: 600px;"
+                     data-action="input->prism#paste keydown->prism#keydownPaste"
+                     data-prism-target="editable">
+                    <code id="yaml" placeholder="Поделитесь своим фрагментом кода тут!">
+                        {{ $content }}
+                    </code>
+                </pre>
 
-                <input name="code" data-prism-target="output" type="hidden" required value="{{ $content }}">
+                <input name="code" 
+                       data-prism-target="output" 
+                       class="w-100 border-0 p-0 position-relative d-block" 
+                       style="height: 0px;" 
+                       required 
+                       value="{{ $content }}">
             </div>
-            <div
-                class="mt-3 d-flex flex-column flex-md-row justify-content-center justify-content-md-start align-items-md-baseline">
+
+            <div class="mt-3 d-flex flex-column flex-md-row justify-content-center justify-content-md-start align-items-md-baseline">
                 <button type="submit" class="btn btn-primary mb-3 mb-md-0">
                     {{ "Опубликовать" }}
                 </button>
 
-                <a href="{{ route('pastebin') }}" type="submit"
+                <a href="{{ route('pastebin') }}" 
+                   type="submit"
                    class="btn btn-link text-decoration-none link-secondary mb-3 mb-md-0">
                     Новый фрагмент
                 </a>
 
-                <div class="d-flex ms-md-auto align-items-baseline justify-content-center justify-content-md-end clipboard" data-controller="clipboard"
+                <div class="d-flex ms-md-auto align-items-baseline justify-content-center justify-content-md-end clipboard" 
+                     data-controller="clipboard"
                      data-clipboard-done-class="done">
                     <small class="user-select-all me-2 col-6 col-md-auto text-truncate lh-1 w-auto"
-                           data-clipboard-target="source">{{ url()->current() }}</small>
-                    <a href="#"
-                       data-action="clipboard#copy">
-                        <x-icon path="i.copy" class="copy-action" data-controller="tooltip"
+                           data-clipboard-target="source">
+                        {{ url()->current() }}
+                    </small>
+                    <a href="#" data-action="clipboard#copy">
+                        <x-icon path="i.copy" 
+                                class="copy-action" 
+                                data-controller="tooltip"
                                 title="Скопировать в буфер"/>
-                        <x-icon path="i.copy-fill" class="copy-done" data-controller="tooltip" title="Скопировано"/>
+                        <x-icon path="i.copy-fill" 
+                                class="copy-done" 
+                                data-controller="tooltip" 
+                                title="Скопировано"/>
                     </a>
                 </div>
             </div>
@@ -54,5 +68,4 @@
             многое другое. Углубитесь в эти ресурсы, чтобы стать выдающимся разработчиком.
         </x-slot>
     </x-call-to-action>
-
 @endsection
