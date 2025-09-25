cfdf4d8695d1564ea24fd941160d2d8ffa54d704
Alexandr Chernyaev <bliz48rus@gmail.com>
2025-09-25 11:20:09 +0300

    Change header banner visibility breakpoint to lg

--- resources/views/components/header-banner-line.blade.php
+++ resources/views/components/header-banner-line.blade.php
@@ -1,3 +1,3 @@
-<div class="bg-primary text-bg-primary bg-gradient text-center py-2 d-none d-xxl-block" id="header-message" data-turbo-permanent>
+<div class="bg-primary text-bg-primary bg-gradient text-center py-2 d-none d-lg-block" id="header-message" data-turbo-permanent>
     {!! $message !!}
 </div>
